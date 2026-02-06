3a191ee12caec5a4f0bb5d8728d7b7c2cc33420b
Thomas Vercamer <tvercame@gmail.com>
2026-02-06 09:27:23 +0100

    Connect React UI to Electron

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -1,19 +1,37 @@
 import { app, BrowserWindow } from 'electron'
 import * as path from 'path'
+import * as url from 'url'
 
 // Global references
 let mainBrowserWindow: BrowserWindow | null = null
-const isDev = process.env.NODE_ENV === 'development'
+const isDev = !!process.env.VITE_DEV_SERVER_URL;
 
 // ---------- WINDOW MANAGEMENT ---------- \\
 function createMainWindow() {
-    const mainWindow = new BrowserWindow({
+    // Determine the URL to load
+    const startURL = isDev
+        ? process.env.VITE_DEV_SERVER_URL!
+        : url.format({
+            pathname: path.join(__dirname, '../../ui/dist/index.html'), // ui/dist
+            protocol: 'file:',
+            slashes: true,
+        });
+
+    // Create the browser window.
+    mainBrowserWindow = new BrowserWindow({
         width: 1200,
         height: 800,
         webPreferences: {
+            // TODO: switch to preload + contextIsolation: true later
             nodeIntegration: true,
             contextIsolation: false,
         },
+    })
+    mainBrowserWindow.loadURL(startURL).then()
+
+    // Handle browser window closed
+    mainBrowserWindow.on('closed', () => {
+        mainBrowserWindow = null;
     })
 }
 
@@ -21,7 +39,6 @@ function createMainWindow() {
 
 // ---------- APP LIFECYLCE MANAGEMENT ---------- \\
 app.whenReady().then(() => {
-    // Setting things up
     createMainWindow()
 
     // Listeners
